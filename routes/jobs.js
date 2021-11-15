362b31bbe61fcd22a6b67a18c4904a98cb11b3ae
Connor Dieck <connordieck@gmail.com>
2021-11-15 12:35:41 -0500

    added jobs routes to app.js and schemas for new and updating jobs

---
+++ routes/jobs.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+// const jobFilterSchema = require("../schemas/jobFilter.json");
+
+const router = new express.Router();
